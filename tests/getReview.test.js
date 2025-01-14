76f1060b6eb706641274d29e0e036d1ac778c5a5
Valerie Racine <116108359+v-racine@users.noreply.github.com>
2025-01-14 15:26:17 -0800

    chore: added error-handling and tests for getReview (#28)

--- tests/getReview.test.js
+++ tests/getReview.test.js
@@ -12,27 +12,14 @@ describe('get a review', () => {
     pgClient: mockPgClient,
   });
 
-  describe('when: users want to view ONE review', () => {
+  describe("when: a user requests a review that doesn't exist", () => {
     let rsp;
     let id = 3;
-
-    let reviews = {
-      rows: [
-        {
-          id: 3,
-          reviewer: 'my chicken',
-          grade: 'c',
-          comments: 'stoopid chicken movie',
-          movie_id: 4,
-          movie_title: 'Point Break',
-        },
-      ],
-    };
 
     beforeEach(async () => {
       mockPgClient.dbQuery = jest.fn().mockImplementation(() => {
         return new Promise((resolve) => {
-          resolve(reviews);
+          resolve({ rows: [] });
         });
       });
 
@@ -43,7 +30,7 @@ describe('get a review', () => {
       jest.clearAllMocks();
     });
 
-    test('then: we return all the information about that ONE review', async () => {
+    test('then: we return the error message AND display the view to create a new review', async () => {
       expect(mockPgClient.dbQuery).toHaveBeenCalledWith(
         'SELECT reviews.*, movie_title FROM reviews JOIN movies ON movies.id = reviews.movie_id WHERE reviews.id = $1',
         id.toString(),
@@ -54,6 +41,51 @@ describe('get a review', () => {
 
       const status = rsp.status;
       expect(status).toBe(200);
+    });
+
+    describe('when: users want to view ONE review', () => {
+      let rsp;
+      let id = 3;
+
+      let reviews = {
+        rows: [
+          {
+            id: 3,
+            reviewer: 'my chicken',
+            grade: 'c',
+            comments: 'stoopid chicken movie',
+            movie_id: 4,
+            movie_title: 'Point Break',
+          },
+        ],
+      };
+
+      beforeEach(async () => {
+        mockPgClient.dbQuery = jest.fn().mockImplementation(() => {
+          return new Promise((resolve) => {
+            resolve(reviews);
+          });
+        });
+
+        rsp = await request(app).get(`/reviews/${id}`).send();
+      });
+
+      afterEach(() => {
+        jest.clearAllMocks();
+      });
+
+      test('then: we return all the information about that ONE review', async () => {
+        expect(mockPgClient.dbQuery).toHaveBeenCalledWith(
+          'SELECT reviews.*, movie_title FROM reviews JOIN movies ON movies.id = reviews.movie_id WHERE reviews.id = $1',
+          id.toString(),
+        );
+
+        const text = rsp.text;
+        expect(text).toMatchSnapshot();
+
+        const status = rsp.status;
+        expect(status).toBe(200);
+      });
     });
   });
 });
